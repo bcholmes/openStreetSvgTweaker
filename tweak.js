e264f6325d383c1323def9007e822904ef679b90
BC Holmes <github@bcholmes.org>
2015-01-09 10:36:46 -0500

    BCH: - include xmldom to parse XML

--- tweak.js
+++ tweak.js
@@ -1,8 +1,16 @@
 var fs = require('fs');
+var DOMParser = require('xmldom').DOMParser;
 
 var tweakFile = function(fileName) {
 	if (fs.existsSync(fileName)) {
 		console.log('tweaking file ' + fileName);
+		
+		var xml = fs.readFileSync(fileName, { "encoding": "UTF-8" });
+//console.log('' + (typeof xml));		
+		var document = new DOMParser().parseFromString(xml);
+		
+		console.log(document.toString());
+		
 	} else {
 		console.log('file ' + fileName + ' does not exist');
 	}	
