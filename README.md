b9ed1c592465f4ac911b4f4ce6da0e21c43f656e
BC Holmes <github@bcholmes.org>
2016-11-30 13:41:49 -0500

    BCH: - add some command-line processing stuff

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# openStreetSvgTweaker
-A simple utility for manipulating SVG files downloaded from OpenStreetMap.org to make them more Inkscape-friendly
+# Open Street SVG Tweaker
+A simple utility for manipulating SVG files downloaded from OpenStreetMap.org to make 
+them more Inkscape-friendly. Importantly, a key part of what that means is moving items 
+on to different Inkscape layers so that I can manipulate them more easily.
+
+This project is implemented as a simple node command-line app. I download the SVG map
+from OpenStreetMap.org and I run the following command:
+
+```
+node tweak.js -o outputFileName.svg inputFileName.svg
+```
+
+Another requirement I have is the need to sometimes convert a map into greyscale. 
+
+```
+node tweak.js -g -o outputFileName.svg inputFileName.svg
+```
+
--- tweak.js
+++ tweak.js
@@ -3,7 +3,7 @@ var xmldom = require('xmldom');
 var DOMParser = xmldom.DOMParser;
 var svgManipulator = require('./svgmanipulator');
 
-var tweakFile = function(fileName) {
+var tweakFile = function(fileName, output) {
 	if (fs.existsSync(fileName)) {
 		console.log('tweaking file ' + fileName);
 		
@@ -16,19 +16,25 @@ var tweakFile = function(fileName) {
 
 		var xs = new (xmldom.XMLSerializer)();
         var documentAsString = xs.serializeToString(document);		
-		fs.writeFileSync("/tmp/sample.svg", documentAsString);
+		fs.writeFileSync(output, documentAsString);
 		
 	} else {
 		console.log('file ' + fileName + ' does not exist');
 	}	
 };
 
+var argv = require('minimist')(process.argv.slice(2));
+console.dir(argv);
+
+var output = argv["o"];
+if (!output) {
+	output = "/tmp/sample.svg";
+}
+
 var happened = false;
-process.argv.forEach(function (val, index, array) {
-	if (index > 1) {
-		tweakFile(val);
-		happened = true;
-	}
+argv["_"].forEach(function (val, index, array) {
+	tweakFile(val, output);
+	happened = true;
 });	
 
 if (!happened) {
